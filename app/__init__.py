6477d043dd0b62b142488800860142008e2fc0e7
tnvrravish <vt7@njit.edu>
2022-04-15 23:00:49 -0400

    Added Click commands, refactored app init file and unittested click commands and added simple pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,57 @@
 """A simple flask web app"""
+import logging
 import os
+from logging.handlers import RotatingFileHandler
+
+import flask_login
 from flask import Flask
-from app.cli import create_database
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
+from app.cli import create_database, create_log_folder
+from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
+from app.simple_pages import simple_pages
 
+login_manager = flask_login.LoginManager()
 
-def page_not_found(e):
-    return render_template("404.html"), 404
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
 
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    # https://flask-login.readthedocs.io/en/latest/  <-login manager
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    # Needed for CSRF protection of form submissions and WTF Forms
+    # https://wtforms.readthedocs.io/en/3.0.x/
+    csrf = CSRFProtect(app)
+    # https://bootstrap-flask.readthedocs.io/en/stable/
+    bootstrap = Bootstrap5(app)
+    # these load functions with web interface
+    app.register_blueprint(simple_pages)
+
+    # these load functionality without a web interface
+    app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
-
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    app.cli.add_command(create_log_folder)
+    db.init_app(app)
+    # Run once at startup:
 
     return app
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
