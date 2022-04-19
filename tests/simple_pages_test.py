b9c33dc093e539a5bb52bbd59c90bf6520504726
tnvrravish <vt7@njit.edu>
2022-04-18 21:31:05 -0400

    adding flask loging and auth, added few simple pages and tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,7 +1,7 @@
 
 def test_request_home_page(application,client):
     """This makes the index page"""
-    response = client.get("/")
+    response = client.get("/home")
     assert response.status_code == 200
     assert b'Home' in response.data
     assert b'Git/GitHub' in response.data
@@ -12,6 +12,16 @@ def test_request_git_page(application,client):
     assert response.status_code == 200
     assert b'Home' in response.data
     assert b'Git/GitHub' in response.data
+
+def test_request_welcome_page(client):
+    response = client.get("/welcome")
+    assert response.status_code == 200
+    assert b'Welcome' in response.data
+
+def test_request_about_page(client):
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b'About' in response.data
 
 def test_page_not_found(client):
     """This Tests Page Not Fount"""
