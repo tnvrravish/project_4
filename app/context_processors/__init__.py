9d0002cfe00c23ac306865b95ffbab817f2d8ad0
tnvrravish <vt7@njit.edu>
2022-04-19 15:41:18 -0400

    edited context processor

--- app/context_processors/__init__.py
+++ app/context_processors/__init__.py
@@ -1,10 +1,11 @@
 from os import getenv
 import datetime
-#from app.auth.forms import login_form
+from app.auth.forms import login_form
 
 
 def utility_text_processors():
     message = "hello world"
+    form = login_form()
 
     def deployment_environment():
         return getenv('FLASK_ENV', None)
@@ -19,6 +20,7 @@ def utility_text_processors():
         return f"{currency}{amount:.2f}"
 
     return dict(
+        form=form,
         mymessage=message,
         deployment_environment=deployment_environment(),
         year=current_year(),
